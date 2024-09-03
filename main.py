2bfba27c5d7bc1c8e1c61e8f0463cec4fd1c5f2b
Muhammad Faisal Farooque <faisalfarooque08@gmail.com>
2024-09-03 13:43:54 +0500

    updates

---
+++ main.py
@@ -0,0 +1,11 @@
+name:str = "muhammad faisal farooque"
+
+#object.method()
+#object.artibute
+
+print( name.capitalize() )
+print( name.casefold() )
+print( name.center(25, "*") )
+print( name.upper() )
+print( name.lower() )
+print( name.title() )
